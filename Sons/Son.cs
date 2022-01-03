26ca405f92b48e3f56ec8b0261868d44a2008e6d
Manon.A <72383929+Atelier-Du-Code@users.noreply.github.com>
2022-01-03 19:59:22 +0100

    Add files via upload

---
+++ Sons/Son.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Media;
+
+
+namespace Definitif_Mathilde_App
+{    
+    public class Son
+    {
+        static public string nomFichier = "";
+        static public string url = "";    
+    }
+
+    class SonSysteme : Son
+    {
+        public void ReponseVraie()
+        {
+            url = "C:\\Jeu_Orthophonie\\Resources\\pluie.wav";
+            SoundPlayer son = new SoundPlayer(url);
+            son.Play();
+
+        }
+        public void ReponseFausse()
+        {
+            url = "C:\\Jeu_Orthophonie\\Resources\\rebond.wav";
+            SoundPlayer son = new SoundPlayer(url);
+            son.Play();
+        }
+    }
+
+
+}
