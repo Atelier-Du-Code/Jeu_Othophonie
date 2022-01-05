9513143aefc50b3bd1343ceafc7cb6a7a2f4ce57
Utilisateur <Utilisateur@DESKTOP-33HGBBG>
2022-01-05 11:10:10 +0100

    Ajout interface brute de la partie Authentification

---
+++ Authentification.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace Definitif_Mathilde_App
+{
+    partial class Authentification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnexion = new System.Windows.Forms.Button();
+            this.txtbIdentifiant = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.lblTitre = new System.Windows.Forms.Label();
+            this.lblIdentifiant = new System.Windows.Forms.Label();
+            this.lblMotDePasse = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnexion
+            // 
+            this.btnConnexion.Location = new System.Drawing.Point(334, 289);
+            this.btnConnexion.Name = "btnConnexion";
+            this.btnConnexion.Size = new System.Drawing.Size(94, 29);
+            this.btnConnexion.TabIndex = 0;
+            this.btnConnexion.Text = "Connexion";
+            this.btnConnexion.UseVisualStyleBackColor = true;
+            // 
+            // txtbIdentifiant
+            // 
+            this.txtbIdentifiant.Location = new System.Drawing.Point(322, 160);
+            this.txtbIdentifiant.Name = "txtbIdentifiant";
+            this.txtbIdentifiant.Size = new System.Drawing.Size(125, 27);
+            this.txtbIdentifiant.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(322, 224);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(125, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(646, 380);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(94, 29);
+            this.btnQuitter.TabIndex = 3;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            // 
+            // lblTitre
+            // 
+            this.lblTitre.AutoSize = true;
+            this.lblTitre.Location = new System.Drawing.Point(322, 63);
+            this.lblTitre.Name = "lblTitre";
+            this.lblTitre.Size = new System.Drawing.Size(115, 20);
+            this.lblTitre.TabIndex = 4;
+            this.lblTitre.Text = "Authentification";
+            // 
+            // lblIdentifiant
+            // 
+            this.lblIdentifiant.AutoSize = true;
+            this.lblIdentifiant.Location = new System.Drawing.Point(334, 137);
+            this.lblIdentifiant.Name = "lblIdentifiant";
+            this.lblIdentifiant.Size = new System.Drawing.Size(77, 20);
+            this.lblIdentifiant.TabIndex = 5;
+            this.lblIdentifiant.Text = "Identifiant";
+            // 
+            // lblMotDePasse
+            // 
+            this.lblMotDePasse.AutoSize = true;
+            this.lblMotDePasse.Location = new System.Drawing.Point(334, 201);
+            this.lblMotDePasse.Name = "lblMotDePasse";
+            this.lblMotDePasse.Size = new System.Drawing.Size(98, 20);
+            this.lblMotDePasse.TabIndex = 6;
+            this.lblMotDePasse.Text = "Mot de passe";
+            // 
+            // Authentification
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMotDePasse);
+            this.Controls.Add(this.lblIdentifiant);
+            this.Controls.Add(this.lblTitre);
+            this.Controls.Add(this.btnQuitter);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtbIdentifiant);
+            this.Controls.Add(this.btnConnexion);
+            this.Name = "Authentification";
+            this.Text = "Authentification";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnexion;
+        private System.Windows.Forms.TextBox txtbIdentifiant;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Label lblTitre;
+        private System.Windows.Forms.Label lblIdentifiant;
+        private System.Windows.Forms.Label lblMotDePasse;
+    }
+}
